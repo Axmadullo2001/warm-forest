9ae51767f54b5b2875e109697544e2db3ab98daf
axmadullo2001TATU <axmadullo2000@gmail.com>
2022-10-17 17:34:38 +0300

    Main page is ready to using

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 import CardList from './components/CardList';
 import Footer from "./components/Footer";
 
-
+import "./assets/var.css";
 import './App.css';
 
 
--- src/mocks/cards.js
+++ src/mocks/cards.js
@@ -1,5 +1,3 @@
-import { v4 } from "uuid"
-
 import img_1 from "../assets/1.jpg"
 import img_2 from "../assets/2.jpg"
 import img_3 from "../assets/3.jpg"
@@ -10,20 +8,20 @@ import img_6 from "../assets/6.jpg"
 import star from "../assets/star.svg";
 
 export const goods = [
-    {id: v4(), name: "Nike Slim Shirt", price: 120, reviews: 0, img: img_1},
-    {id: v4(), name: "Puma Slim Pant", price: 65, reviews: 0, img: img_2},
-    {id: v4(), name: "Lacoste Free Shirt", price: 220, reviews: 0, img: img_3},
-    {id: v4(), name: "Adidas Fit Shirt", price: 100, reviews: 0, img: img_4},
-    {id: v4(), name: "Adidas Fit Pant", price: 139, reviews: 0, img: img_5},
-    {id: v4(), name: "Nike Slim Shirt", price: 78, reviews: 0, img: img_6},
+    {id: 1, name: "Nike Slim Shirt", price: 120, reviews: 0, img: img_1},
+    {id: 2, name: "Puma Slim Pant", price: 65, reviews: 0, img: img_2},
+    {id: 3, name: "Lacoste Free Shirt", price: 220, reviews: 0, img: img_3},
+    {id: 4, name: "Adidas Fit Shirt", price: 100, reviews: 0, img: img_4},
+    {id: 5, name: "Adidas Fit Pant", price: 139, reviews: 0, img: img_5},
+    {id: 6, name: "Nike Slim Shirt", price: 78, reviews: 0, img: img_6},
 ];
 
 
 
 export const config = [
-    {id: v4(), src: star},
-    {id: v4(), src: star},
-    {id: v4(), src: star},
-    {id: v4(), src: star},
+    {id: 1, src: star},
+    {id: 2, src: star},
+    {id: 3, src: star},
+    {id: 4, src: star},
 ];
 
