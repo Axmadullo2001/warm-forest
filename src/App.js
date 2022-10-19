5acd61f43203bf5854d2da09df1297bd404be7cd
axmadullo2001TATU <axmadullo2000@gmail.com>
2022-10-19 11:40:36 +0300

    Layout of Sign in page

--- src/App.js
+++ src/App.js
@@ -3,23 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import CardList from './components/CardList';
 import Footer from "./components/Footer";
-
 import { SignIn } from "./components/SignIn";
 
-import "./assets/var.css";
 import './App.css';
+import "./assets/var.css";
 
 const App = () => {
 
   return (
     <div className="App">
       <Header />
-
       <Routes>
           <Route path="/" element={ <CardList /> } />
           <Route path="/sign-in" element={ <SignIn /> } />
       </Routes>
-
       <Footer />
     </div>
   );
