1bf0c2eed59e825740c1e79fcd2174371c7e7925
axmadullo2001TATU <axmadullo2000@gmail.com>
2022-11-02 21:54:17 +0300

    Debugging...

--- src/components/CardItem/index.jsx
+++ src/components/CardItem/index.jsx
@@ -6,6 +6,7 @@ import { NavLink } from 'react-router-dom'
 import { config } from '../../mocks'
 
 const CardItem = (props) => {
+    console.log(props)
     const { img, name, price, reviews } = props
 
     return (
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import CardItem from '../../components/CardItem'
 import Header from '../../components/Header'
+import {Pagination} from '../../components/Pagination'
 import SearchBox from '../../components/SearchBox'
 import Footer from '../../components/Footer'
-import {Pagination} from '../../components/Pagination'
 
-
+import { url } from '../../api/example'
 
 import s from './styles.module.scss'
 
@@ -14,12 +15,32 @@ import s from './styles.module.scss'
 const HomePage = () => {
     const [ searchedData, setSearchedData ] = useState([])
     const [ searchFilter, setSearchFilter ] = useState('')
+    const [ currentPage, setCurrentPage ] = useState(1)
+    const [ totalPages, setTotalPages] = useState(4)
 
     useEffect(() => {
         const filteredPeople = searchedData.filter(n => n.first_name.toLowerCase().includes(searchFilter.toLowerCase()))
         setSearchedData(filteredPeople)
     }, [searchFilter])
 
+    axios.get(`${url}users`, {
+        params: {
+            per_page: 3,
+            page: currentPage
+        }
+      })
+        .then((response) => {
+          setTotalPages(response.data.total_pages)
+          setSearchedData(response.data.data)
+        })
+    , [currentPage, searchFilter]
+
+    const searchItems = () => {
+        if (searchFilter) {
+            const filteredPeople = searchedData.filter(n => n.first_name.toLowerCase().includes(searchFilter.toLowerCase()))
+            setSearchedData(filteredPeople)
+        }
+    }
 
     return (
         <>
@@ -27,11 +48,15 @@ const HomePage = () => {
             <SearchBox
             setSearchFilter={setSearchFilter}
             searchFilter={searchFilter}
+            searchItems={searchItems}
             />
             <div className={s.card_list_container}>
                 {searchedData.map(item => <CardItem key={item.id} {...item} />)}
             </div>
             <Pagination
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+              totalPages={totalPages}
             />
             <Footer />
         </>
