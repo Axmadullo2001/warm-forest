6878b3080bcbde9085b15685cece2bdee8bd6c0f
axmadullo2001TATU <axmadullo2000@gmail.com>
2022-11-02 13:33:53 +0300

    Pagination done, search failed

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,39 +1,44 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import CardItem from '../../components/CardItem'
 import Header from '../../components/Header'
 import SearchBox from '../../components/SearchBox'
 import Footer from '../../components/Footer'
-
-import { goods } from '../../mocks'
-
-import s from './styles.module.scss'
-import axios from 'axios'
 import {Pagination} from '../../components/Pagination'
 
-const url = 'https://reqres.in/api/'
+import { url } from '../../api/example'
+
+import s from './styles.module.scss'
+
 
 const HomePage = () => {
-    const [ searchedData, setSearchedData ] = useState(goods)
+    const [ searchedData, setSearchedData ] = useState([])
     const [ searchFilter, setSearchFilter ] = useState('')
     const [ currentPage, setCurrentPage ] = useState(1)
-    const [ totalPages, setTotalPages] = useState(1)
+    const [ totalPages, setTotalPages] = useState(4)
 
     useEffect(() => {
 
       axios.get(`${url}users`, {
         params: {
-          page: currentPage
+            per_page: 3,
+            page: currentPage
         }
       })
         .then((response) => {
           setTotalPages(response.data.total_pages)
+          setSearchedData(response.data.data)
       })
 
-        const filteredPeople = goods.filter(n => n.name.toLowerCase().includes(searchFilter.toLowerCase()))
-        setSearchedData(filteredPeople)
     }, [currentPage, searchFilter])
 
+    const searchItems = () => {
+        if (searchFilter) {
+            const filteredPeople = searchedData.filter(n => n.first_name.toLowerCase().includes(searchFilter.toLowerCase()))
+            setSearchedData(filteredPeople)
+        }
+    }
 
     return (
         <>
@@ -41,6 +46,7 @@ const HomePage = () => {
             <SearchBox
             setSearchFilter={setSearchFilter}
             searchFilter={searchFilter}
+            searchItems={searchItems}
             />
             <div className={s.card_list_container}>
                 {searchedData.map(item => <CardItem key={item.id} {...item} />)}
