31a3a3a8bba1321a1b9e6fd5470a2dc4cd7a295b
axmadullo2001TATU <axmadullo2000@gmail.com>
2022-11-02 21:55:11 +0300

    Debugging...

--- src/components/CardItem/index.jsx
+++ src/components/CardItem/index.jsx
@@ -1,39 +1,37 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 import s from './styles.module.scss'
-import { NavLink } from 'react-router-dom'
-import { config } from '../../mocks'
 
 const CardItem = (props) => {
-    console.log(props)
-    const { img, name, price, reviews } = props
+    const { email, first_name, last_name, avatar } = props
 
     return (
         <div className={s.card_item}>
             <div className={s.card_item__img_block}>
-                <img className={s.card_item__img} src={ img } alt={ name } />
+                <img className={s.card_item__img} src={ avatar } alt={ name } />
             </div>
 
             <div className={s.card_item__body}>
-                <p className={s.card_item__name}><NavLink to="/"  className={s.card_item__name_link}>{ name }</NavLink></p>
-                <div className={s.card_item__reviews}>
+                <p className={s.card_item__name}><NavLink to="/"  className={s.card_item__name_link}>{ first_name } { last_name }</NavLink></p>
+                {/* <div className={s.card_item__reviews}>
                     {config.map(star => (
                         <img className={s.card_item__star} key={ star.id } src={ star.src } alt={''} />
                     ))}
                     <span className={s.card_item__reviews}>{ reviews } reviews</span>
-                </div>
-                <p className={s.card_item__price}>${ price }</p>
+                </div> */}
+                <p className={s.card_item__price}>{ email }</p>
             </div>
         </div>
     )
 }
 
 CardItem.propTypes = {
-    img: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    reviews: PropTypes.number.isRequired
+    email: PropTypes.string,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+    avatar: PropTypes.string
 }
 
 export default CardItem
